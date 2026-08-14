247a35d75f98380a92ff5e417684dd269c5b5e8a
CENSOR37 <viixxivvimm@gmail.com>
2026-08-15 03:13:32 +0700

    add nova common helper

---
+++ Plugins/NovaFramework/Source/NovaCore/Common.h
@@ -0,0 +1,49 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "GameFramework/Actor.h"
+
+namespace Nova
+{
+	template <typename T>
+	T* Get(const AActor* Actor)
+	{
+		static_assert(TIsDerivedFrom<T, UActorComponent>::IsDerived, "T must derive from UActorComponent");
+
+		if (IsValid(Actor))
+		{
+			return Actor->FindComponentByClass<T>();
+		}
+
+		return nullptr;
+	}
+
+	template <typename T>
+	T* Get(const AActor* Actor, const FName Name = NAME_None)
+	{
+		static_assert(TIsDerivedFrom<T, UActorComponent>::IsDerived, "T must derive from UActorComponent");
+
+		if (IsValid(Actor))
+		{
+			if (Name.IsNone())
+			{
+				return Actor->FindComponentByClass<T>();
+			}
+
+			for (UActorComponent* Comp : Actor->GetComponents())
+			{
+				if (Comp == nullptr)
+					continue;
+
+				if (Comp->GetFName() != Name)
+					continue;
+
+				if (T* TypedComp = Cast<T>(Comp))
+					return TypedComp;
+			}
+		}
+
+		return nullptr;
+	}
+}
